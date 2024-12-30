014adc7c6ab73d9661c7db4878fb967e2a596d50
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-30 15:42:29 +0100

    [PRJ0002] refactor code of dependencies and modules

--- SpringCinemaApplication/core-film/src/main/java/com/tokioschool/filmapp/security/PasswordEncoderConfiguration.java
+++ SpringCinemaApplication/core-film/src/main/java/com/tokioschool/filmapp/security/PasswordEncoderConfiguration.java
@@ -2,6 +2,7 @@ package com.tokioschool.filmapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +12,10 @@ public class PasswordEncoderConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Configuration
+    @EnableMethodSecurity
+    public static class ServiceSecurityConfiguration {
     }
 }
