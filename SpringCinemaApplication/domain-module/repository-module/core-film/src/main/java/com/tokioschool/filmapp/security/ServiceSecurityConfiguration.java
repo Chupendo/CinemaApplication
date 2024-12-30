4f620ff8a65916ebac0c87e3be48a70d881d2bf4
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-30 15:26:07 +0100

    [PRJ0002] configuration basic of security

---
+++ SpringCinemaApplication/domain-module/repository-module/core-film/src/main/java/com/tokioschool/filmapp/security/ServiceSecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.tokioschool.filmapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class ServiceSecurityConfiguration {
+}
