7dbfa5e5c18fa620f4a28bd5c2061ef2d6ca6fde
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-31 10:34:27 +0100

    [PRJ0003] created domain, repository and schema of bbdd for roles

---
+++ SpringCinemaApplication/domain-module/src/main/java/com/tokioschool/filmapp/domain/Role.java
@@ -0,0 +1,22 @@
+package com.tokioschool.filmapp.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+}
