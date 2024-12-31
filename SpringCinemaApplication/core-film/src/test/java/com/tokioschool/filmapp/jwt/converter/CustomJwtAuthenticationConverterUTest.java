2295cbd444ba61d3c0c583e1905a725af8420465
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-31 18:35:51 +0100

    [PRJ0003] refactor code custom wjt authentication converter more test

---
+++ SpringCinemaApplication/core-film/src/test/java/com/tokioschool/filmapp/jwt/converter/CustomJwtAuthenticationConverterUTest.java
@@ -0,0 +1,49 @@
+package com.tokioschool.filmapp.jwt.converter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.List;
+
+class CustomJwtAuthenticationConverterUTest {
+
+    private final CustomJwtAuthenticationConverter converter = new CustomJwtAuthenticationConverter();
+
+    @Test
+    void givenAJwtWithAuthorities_whenConvert_thenReturnAuthenticationToken() {
+        // Arrange
+        Jwt mockJwt = Mockito.mock(Jwt.class);
+
+        // Simular el payload del JWT con el claim "authorities"
+        Mockito.when(mockJwt.getClaim("authorities")).thenReturn(List.of("ROLE_ADMIN", "ROLE_USER"));
+
+        // Act
+        AbstractAuthenticationToken authenticationToken = converter.convert(mockJwt);
+
+        // Assert
+        Assertions.assertEquals(2, authenticationToken.getAuthorities().size());
+        Assertions.assertEquals(
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER")),
+                authenticationToken.getAuthorities().stream().toList()
+        );
+    }
+
+    @Test
+    void givenAJwtWithOutAuthorities_whenConvert_thenReturnAuthenticationTokenEmpty() {
+        // Arrange
+        Jwt mockJwt = Mockito.mock(Jwt.class);
+
+        // Simular el payload del JWT con el claim "authorities"
+        Mockito.when(mockJwt.getClaim("authorities")).thenReturn(null);
+
+        // Act
+        AbstractAuthenticationToken authenticationToken = converter.convert(mockJwt);
+
+        // Assert
+        Assertions.assertEquals(0, authenticationToken.getAuthorities().size());
+    }
+}
